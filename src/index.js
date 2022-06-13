c9f722045d956e4d3338d89601417925495cee52
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-13 16:55:50 -0500

    Update store before react renders the project

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Profile from './components/Profile';
 import RocketCard from './components/RocketCard';
+import { GetRocketAPI } from './redux/rockets/rockets';
+import configureStore from './redux/configureStore';
 
+configureStore.dispatch(GetRocketAPI());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <React.StrictMode>
-      <Header />
-      <Routes>
-        <Route path="/" element={<RocketCard />} />
-        <Route path="Profile" element={<Profile />} />
-      </Routes>
-    </React.StrictMode>
-  </Router>,
+  <Provider store={configureStore}>
+    <Router>
+      <React.StrictMode>
+        <Header />
+        <Routes>
+          <Route path="/" element={<RocketCard />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+      </React.StrictMode>
+    </Router>
+  </Provider>,
 );
