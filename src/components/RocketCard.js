84cfa6012a538d9596b656c4d535c7a7e598452f
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-14 09:40:01 -0500

    Display rockets and add styling

--- src/components/RocketCard.js
+++ src/components/RocketCard.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './RocketCard.css';
+import { useSelector } from 'react-redux';
 
-const RocketCard = () => (
-  <div className="rocket-card">
-    <img className="img-rocket" src="#" alt="rocket" />
-    <div>
-      <p className="name-rocket">Name</p>
-      <p className="info-rocket">Info</p>
-      <button className="reserve-rocket" type="button">Reserve Rocket</button>
-    </div>
-  </div>
-);
-
+const RocketCard = () => {
+  const rocketlist = useSelector((state) => state.RocketReducer);
+  return (
+    rocketlist.map((element) => (
+      <div className="rocket-card" key={element.id}>
+        <img className="img-rocket" src={element.flickr_images} alt="rocket" />
+        <div className="rocket-div">
+          <p className="name-rocket">{element.name}</p>
+          <p className="info-rocket">{element.info}</p>
+          <button className="reserve-rocket" type="button">Reserve Rocket</button>
+        </div>
+      </div>
+    ))
+  );
+};
 export default RocketCard;
