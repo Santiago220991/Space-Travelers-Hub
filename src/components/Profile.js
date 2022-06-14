56cfcc9f8416d7bd345e3f9f0b00799127e84744
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-14 15:56:07 -0500

    Add feature to show a list of rockets reserved

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div>Hola</div>
-);
-
+const Profile = () => {
+  const rocketlist = useSelector((state) => state.rocket);
+  console.log(rocketlist);
+  const newrocketlist = rocketlist.filter((element) => element.reserved === true);
+  return (
+    <div>
+      <div>Missions</div>
+      <div className="rockets-reserved-list">
+        <p>Rockets</p>
+        {newrocketlist.map((element) => (
+          <div className="rocket-reserved" key={element.name}>{element.name}</div>))}
+      </div>
+    </div>
+  );
+};
 export default Profile;
