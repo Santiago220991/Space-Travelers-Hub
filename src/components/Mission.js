b773a8382c4530ad824631e515f126f839833e00
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-06-14 18:31:38 -0300

    add conditional rendering

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -9,17 +9,29 @@ function Mission(props) {
     dispatch(ToggleMissionAction(id));
   }
   const {
-    id, name, description,
+    id, name, description, member,
   } = props;
   return (
     <div className="row" id={id}>
       <div className="column1 MissionName cell">{name}</div>
       <div className="column2 MissionDesc cell">{description}</div>
+      {member && (
       <div className="column3 cell"><span className="ActiveMember">Active Member</span></div>
+      )}
+      {!member && (
+      <div className="column3 cell"><span className="NotAMember">NOT A MEMBER</span></div>
+      )}
       <div className="column4 cell">
+        {member && (
         <button type="button" className="LeaveMission" onClick={ToggleMission}>
           Leave Mission
         </button>
+        )}
+        {!member && (
+        <button type="button" className="JoinMission" onClick={ToggleMission}>
+          Join Mission
+        </button>
+        )}
       </div>
     </div>
   );
@@ -30,3 +42,4 @@ export default Mission;
 Mission.propTypes = { id: PropTypes.string.isRequired };
 Mission.propTypes = { name: PropTypes.string.isRequired };
 Mission.propTypes = { description: PropTypes.string.isRequired };
+Mission.propTypes = { member: PropTypes.bool.isRequired };
